c88fa08770b7865316cc7e503eab8922c63644b0
Hardik Desai <hardikd@zignuts.com>
2023-11-27 11:56:28 +0530

    fix: middleware

--- index.js
+++ index.js
@@ -1,78 +1,63 @@
-// // ** Dotenv Config
-// require("dotenv").config();
+// ** Dotenv Config
+require("dotenv").config();
 
-// // ** Packages
-// const express = require('express');
-// const mongoose = require('mongoose');
+// ** Packages
+const express = require('express');
+const mongoose = require('mongoose');
 
-// // ** Config
-// const app = express()
-// const uri = process.env.DB_URL;
+// ** Config
+const app = express()
+app.use(express.json());
+const uri = process.env.DB_URL;
 
-// mongoose
-//     .connect(uri)
-//     .then(() => console.log("Database connected!"))
-//     .catch(err => console.log(err));
+mongoose
+    .connect(uri)
+    .then(() => console.log("Database connected!"))
+    .catch(err => console.log(err));
 
-// // ** Constant
-// const PORT = 5000;
+// ** Constant
+const PORT = 5000;
 
-// const restaurantSchema = new mongoose.Schema({
-//     address: {
-//         type: {
-//             borough: {
-//                 type: String,
-//                 required: true,
-//             },
-//             cuisine: {
-//                 type: String,
-//                 required: true,
-//             },
-//         },
-//         required: true,
-//     },
-//     grades: [
-//         {
-//             name: {
-//                 type: String,
-//                 required: true,
-//             },
-//             restaurant_id: {
-//                 type: String,
-//                 required: true,
-//             },
-//         },
-//     ],
-// });
-
-// const Restaurant = mongoose.model('Restaurant', restaurantSchema);
-
-// app.get('/', async (req, res) => {
-//     const list = await Restaurant.find()
-//     if (list) {
-//         res.status(200).json({ restaurant: list })
-//     }
-//     else { res.status(400).json({ message: 'Failed To Retrieve Blogs' }) }
-// })
-
-// // ** Server
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port http://localhost:${PORT}`)
-// })
-
-// module.exports = app;
-
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Express on Vercel");
+const restaurantSchema = new mongoose.Schema({
+    address: {
+        type: {
+            borough: {
+                type: String,
+                required: true,
+            },
+            cuisine: {
+                type: String,
+                required: true,
+            },
+        },
+        required: true,
+    },
+    grades: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            restaurant_id: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
 });
 
-app.listen(5000, () => {
-    console.log("Running on port 5000.");
-});
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
-// Export the Express API
-module.exports = app;
+app.get('/', async (req, res) => {
+    const list = await Restaurant.find()
+    if (list) {
+        res.status(200).json({ restaurant: list })
+    }
+    else { res.status(400).json({ message: 'Failed To Retrieve Blogs' }) }
+})
+
+// ** Server
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+})
+
